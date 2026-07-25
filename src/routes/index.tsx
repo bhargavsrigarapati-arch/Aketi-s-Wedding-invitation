2aa89e9755ce32b18b131679132497a56a10302e
bhargavsrigarapati-arch <bhargavsri.garapati@gmail.com>
2026-07-25 14:32:30 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -245,8 +245,7 @@ function Opening({ onOpen }: { onOpen: () => void }) {
         >
           <div className="mx-auto h-px w-40 divider-gold" />
           <div className="mt-6 font-[var(--font-script)] text-4xl shimmer-text sm:text-5xl">
-            Aketi's Wedding Invitaion
-            
+            AKETI'S WEDDING INVITATION
           </div>
           <div className="mx-auto mt-6 h-px w-40 divider-gold" />
         </motion.div>
